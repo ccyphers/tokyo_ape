358813695b8c62e038b3f3c92c2814c20a122d4c
Cliff Cyphers <ccyphers@Cliffs-MacBook-Pro.local>
2017-03-16 07:09:45 -0500

    update add api example to match kong 0.10.0 updates

--- README.md
+++ README.md
@@ -22,14 +22,20 @@ Tokyo Ape.
     INPUT
     Object containing any valid options to POST http://kong_admin/apis
 
-    OUTPUT:
-     { upstream_url: 'https://www.google.com',
-     strip_request_path: true,
-     request_path: '/google',
-     id: '2c21fb14-5785-4851-922d-a2bf5660415e',
-     created_at: 1475957149000,
+    { methods: [ 'GET' ],
+     http_if_terminated: true,
+     id: '9bbf8118-5b8e-447d-afc3-9bf782b2807c',
+     retries: 5,
      preserve_host: false,
-     name: 'google' }
+     created_at: 1489665806709,
+     upstream_connect_timeout: 60000,
+     upstream_url: 'https://www.google.com',
+     uris: [ '/google' ],
+     https_only: false,
+     upstream_send_timeout: 60000,
+     upstream_read_timeout: 60000,
+     name: 'google',
+     strip_uri: true }
 
      */
 
--- examples/add_api.js
+++ examples/add_api.js
@@ -7,17 +7,24 @@ INPUT
 Object containing any valid options alled to POST http://kong_admin/apis
 
 OUTPUT:
- { upstream_url: 'https://www.google.com',
- strip_request_path: true,
- request_path: '/google',
- id: '2c21fb14-5785-4851-922d-a2bf5660415e',
- created_at: 1475957149000,
+ { methods: [ 'GET' ],
+ http_if_terminated: true,
+ id: '9bbf8118-5b8e-447d-afc3-9bf782b2807c',
+ retries: 5,
  preserve_host: false,
- name: 'google' }
+ created_at: 1489665806709,
+ upstream_connect_timeout: 60000,
+ upstream_url: 'https://www.google.com',
+ uris: [ '/google' ],
+ https_only: false,
+ upstream_send_timeout: 60000,
+ upstream_read_timeout: 60000,
+ name: 'google',
+ strip_uri: true }
 
  */
 
-kong.api.add({name: 'google', upstream_url: 'https://www.google.com', request_path: '/google', strip_request_path: true})
+kong.api.add({name: 'google', methods: 'GET', upstream_url: 'https://www.google.com', uris: '/google', strip_uri: true})
     .then(function(res) {
         console.log(util.inspect(res.body));
     });
